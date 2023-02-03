abccd0e9eb135a97457f074e9d9e68f7a0a89041
VedantSharma11 <vedants2003@gmail.com>
2023-02-03 19:39:01 +0530

    Updates

---
+++ precache-manifest.30d32dbcc6761e7f06a9014b42c5bbf5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866f4b58024ab9c1c1158f18d5aa9728",
+    "url": "/ReactNote-App/index.html"
+  },
+  {
+    "revision": "bd944ea4a179057518bc",
+    "url": "/ReactNote-App/static/js/2.dc8daa36.chunk.js"
+  },
+  {
+    "revision": "c014e16507e217ce795e",
+    "url": "/ReactNote-App/static/js/main.6fbd1455.chunk.js"
+  },
+  {
+    "revision": "c7c56d0e2204d77e051c",
+    "url": "/ReactNote-App/static/js/runtime-main.6394bfcb.js"
+  }
+]);
